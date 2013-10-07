674e260d4096bb0e306a5ffaf7bacaf971384a82
Zino <zinoxx@live.se>
2013-10-07 15:38:33 +0200

    style update

--- viewsource.php
+++ viewsource.php
@@ -50,10 +50,10 @@ div.code code, div.rows code {
 
 }
 
-/*section#content p code {
-	font-size: 1.2em;
+section#content p code {
+	font-size: 1em;
 	font-family: 'Inconsolata', Arial, sans-serif;
-}*/
+}
 
 EOD;
 
