ff53c9b8f74b07596715e3008378bdcd4f2014d0
Zino <zinoxx@live.se>
2013-10-07 16:20:13 +0200

    validation fixes

--- incl/config.php
+++ incl/config.php
@@ -60,15 +60,21 @@ EOD;
 
 //GetCurrentURL-function with courtesy of: http://webcheatsheet.com/PHP/get_current_page_url.php
 function getCurrentUrl() {
- $pageURL = 'http';
- if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
- return $pageURL;
+$pageURL = 'http';
+if(isset($_SERVER["HTTPS"]))
+{
+	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+}
+
+$pageURL .= "://";
+
+if ($_SERVER["SERVER_PORT"] != "80") {
+	$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+} else {
+	$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+}
+
+return $pageURL;
 }
 
 ?>
