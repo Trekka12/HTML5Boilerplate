308b150ffc49abc3d7548ea8b0031aa4fa3d77bf
Zino <zinoxx@live.se>
2013-10-07 13:42:47 +0200

    updated viewsource.php

--- viewsource.php
+++ viewsource.php
@@ -8,13 +8,15 @@ $currentURL = getCurrentURL();
 $pageTitle = "Källkodsvisnings verktyg för min sida";
 $page_Meta_Desc = "Denna sida innehåller ett källkodsvisnings verktyg.";
 
+//Include the source-code investigative script
+include('source.php');
+
 //CSS Styles for this page uniquely
-$style = <<<EOD
+$style = $sourceStyle;
 
-EOD;
+//The page contents for this page uniquely
+$pageContent = $sourceBody;
 
-//the page contents for this page uniquely
-$pageContent = include('source.php');
 
 //Add the index.php contents to the default template
 include('theme/default_template.php');
