5e68a8ec2524d0a3fe94d6419831bf7f2de92e24
Zino <zinoxx@live.se>
2013-10-07 14:35:33 +0200

    add contents to site

--- index.php
+++ index.php
@@ -23,7 +23,31 @@ $pageContent = <<<EOD
 
 <!-- Här hamnar sedan allt presentations innehåll i HTML-kod -->
 <h1>Presentation av mig</h1>
-<p></p>
+<p>
+Mitt namn är André och jag är 21 år och bor i Helsingborg på den sydvästra skånska kusten.<br><br>
+
+För nuvarande trimmar jag min expertis och mina kunskaper inom varierande ämne och områden 
+(mestadels akademiskt) vilka för det mesta kan klassas inom datorer, webb, teknik och utveckling 
+medan jag har uppehåll från Lunds Tekniska Högskola's civilingenjörsutbildning 
+inom Informations- och kommunikationsteknologi.<br><br>
+
+Har redan lyckats få en fot in i dörren hos flera intressanta och användbara och roliga ämne såsom; 
+Java, PHP, HTML 5, CSS 3.0, SQLite och Databaser, webbutveckling och webbdesign (back-end/front-end), 
+grafisk design, verktyg och tjänster ute på nätet idag m.m.<br><br>
+
+I övrigt gillar jag att träna och att vara ute i naturen, gillar att jogga, powerwalka/hajka, 
+spela pingis, tennis, simma, styrketräna/gymma, m.m.<br><br>
+
+Några andra fritidsintressen inkluderar bl. a.; egna projekt och egen projektutveckling, 
+film och tv-serier, musik och datorer i allmänhet, samt fotografering.<br><br>
+
+Tar ett fåtal uppdrag lite då och då på mitt egna företag, oftast inom datorservice/support, 
+webbutveckling och övrig försäljning av tekniska tjänster och konsultation inom mina aktuella 
+expertisområden.<br><br>
+
+Min stora förhoppning är att någon gång i framtiden när utbildningen är klar kunna vara del av eller
+driva ett eget företag inom webb, program och spel-utveckling såväl som ev. andra datortjänster som hör till.
+</p>
 EOD;
 
 //Add the index.php contents to the default template
